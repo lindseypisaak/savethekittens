ceb58c7f65a59b84c1a28a40d9b2e50cf9e80bd2
Lindsey Isaak <lindseyisaak@lindseys-imac.local>
2016-10-18 21:34:11 -0600

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Lindsey Isaak on 2016-10-18.
+//  Copyright © 2016 Lindsey Isaak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
